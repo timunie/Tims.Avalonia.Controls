81d01d7b08be9bbff6de28b191f82ce130e2224d
Tim <tim.uniewski@gmail.com>
2021-10-05 09:15:00 +0200

    Implement ColorToTransparencyColorConverter

---
+++ src/TimsAvaloniaControls/Converters/ColorToTransparencyColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace TimsAvaloniaControls.Converters
+{
+    public class ColorToTransparencyColorConverter : IValueConverter
+    {
+        private static ColorToTransparencyColorConverter _Instance;
+        public static ColorToTransparencyColorConverter Instance => _Instance ??= new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var inputColor = (Color)value;
+                var transparencyLevel = (double)(new DoubleConverter().ConvertFromInvariantString(parameter?.ToString()));
+
+                if (transparencyLevel < 0) transparencyLevel = 0;
+                if (transparencyLevel > 1) transparencyLevel = 1;
+
+                transparencyLevel = 1 - transparencyLevel;
+
+                return new Color((byte)(transparencyLevel * inputColor.A), inputColor.R, inputColor.G, inputColor.B);
+            }
+            catch
+            {
+                return BindingOperations.DoNothing;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
